b9a47660f6f2b5426c7ee8fe085975e2a764168f
Denis Lisenkov <lisenkov.d.y@gmail.com>
2023-02-22 22:21:18 +0300

    Renamed project

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,15 +1,15 @@
 <template>
-    <div class="header">
-        <h1>Remote file system</h1>
-    </div>
+  <div class="header">
+    <h1>Local-file-system</h1>
+  </div>
 </template>
 
 <style scoped>
 .header {
-    text-align: center;
-    padding: 30px;
-    background: #646964;
-    color: white;
-    text-transform: uppercase;
+  text-align: center;
+  padding: 30px;
+  background: #646964;
+  color: white;
+  text-transform: uppercase;
 }
 </style>
